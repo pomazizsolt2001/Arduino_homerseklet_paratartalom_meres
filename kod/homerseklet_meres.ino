047bd84d69b961577ddbf1cc826ac0764fba473b
pomazizsolt2001 <pomazizsolt2001@gmail.com>
2026-03-23 12:06:31 +0100

    Update homerseklet_meres.ino

--- kod/homerseklet_meres.ino
+++ kod/homerseklet_meres.ino
@@ -1,1 +1,26 @@
+#include <DHT.h>
 
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  Serial.print("Homerseklet: ");
+  Serial.print(temperature);
+  Serial.println(" C");
+
+  Serial.print("Paratartalom: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  delay(2000);
+}
